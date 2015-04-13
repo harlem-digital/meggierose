e1f3c9bd391a28d6ce7eefa644f2aa352ca35023
WyattMeade <wyatt.meade@gmail.com>
2015-04-12 23:13:03 -0400

    404 css and page

--- website/wp-content/themes/memorabella/404.php
+++ website/wp-content/themes/memorabella/404.php
@@ -9,22 +9,10 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section>
+		<p>BUMMER. I DIDN'T TAKE ANY PHOTOS OF THAT.<br>TRY SOMETHING ELSE!</p>
+</section>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
