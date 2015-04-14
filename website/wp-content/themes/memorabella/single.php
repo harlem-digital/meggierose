107fb8575fc9c09840787350475f3a5e9791ca32
KennethJackson <kenneth@whoiskenjackson.com>
2015-04-13 22:34:09 -0700

    removing pagination from single

--- website/wp-content/themes/memorabella/single.php
+++ website/wp-content/themes/memorabella/single.php
@@ -23,16 +23,6 @@ get_header(); ?>
 			 */
 			get_template_part( 'content', get_post_format() );
 
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
-
 		// End the loop.
 		endwhile;
 		?>
