6fba3b2095346d3eb8efc7b936c9ad965359f952
KennethJackson <kenneth@whoiskenjackson.com>
2015-04-13 22:32:17 -0700

    removing comments

--- website/wp-content/themes/memorabella/single.php
+++ website/wp-content/themes/memorabella/single.php
@@ -23,11 +23,6 @@ get_header(); ?>
 			 */
 			get_template_part( 'content', get_post_format() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 			// Previous/next post navigation.
 			the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
