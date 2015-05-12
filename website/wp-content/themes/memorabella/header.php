72689834fa9a0d3a71e7cdb63d685047f51eabe8
KennethJackson <kenneth@whoiskenjackson.com>
2015-05-11 21:28:54 -0700

    updating favicon, about text, and form search bar bug

--- website/wp-content/themes/memorabella/header.php
+++ website/wp-content/themes/memorabella/header.php
@@ -14,6 +14,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="icon" type="image/png" href="favicon.png">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
